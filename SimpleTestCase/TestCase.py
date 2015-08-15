1cc2c9ae891cf2a79e9f3dbff8c1df9220e4d6ea
williamhuang <chobits5468@gmail.com>
2015-08-16 01:51:22 +0800

    [add] write testcase for new feature

--- SimpleTestCase/TestCase.py
+++ SimpleTestCase/TestCase.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
+
 	def setUp(self):
 		self.browser = webdriver.Chrome('chromedriver_win32/chromedriver.exe')
 		self.browser.implicitly_wait(3)
@@ -11,9 +13,26 @@ class NewVisitorTest(unittest.TestCase):
 
 	def test_case(self):
 		self.browser.get('http://localhost:8000')
-		self.assertIn('To-Do', self.browser.title)
-		#assert 'To-Do' in browser.title, "Browser title was " + browser.title
-		self.fail('Finish the test!')
+		print (self.browser.title)
+		self.assertIn('hello', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('hello', header_text)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+				inputbox.get_attribute('placeholder'),
+				'Enter a To-Do item'
+		)
+
+		inputbox.send_keys('Buy peacock feathers')
+
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_tag_name('tr')
+		self.assertTrue(any(row.text =='1: Buy peacock feathers' for row in rows))
+		self.fail('End the test')
+
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
